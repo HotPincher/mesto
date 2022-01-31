869fe0a70ce0bf7f1185dd369aa5864e14701e44
HotPincher <92081461+HotPincher@users.noreply.github.com>
2022-01-31 22:00:10 +1000

    provided code amendment after review

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@
 
 // 1 Initial rendering of cards in __elements section
 
-const cardContentTitles = [
-  {title: 'Марианская впадина', link: './images/image_mariana.jpg'},
+const cardContentTitles = 
+[ {title: 'Марианская впадина', link: './images/image_mariana.jpg'},
   {title: 'Филиппинский архипелаг', link: './images/image_phillipines.jpg'},
   {title: 'Красное море', link: './images/image_red-sea.jpg'},
   {title: 'Большой Барьерный Риф', link: './images/image_reef.jpg'},
@@ -11,23 +11,51 @@ const cardContentTitles = [
   {title: 'Карибское море', link: './images/image_carribean.jpg'},
 ]
 
+const popupTitle = 
+['Редактировать профиль', 'Новое место']
+
+const popupFormPlaceholder = 
+[{inputOne: 'Имя исследователя', inputTwo: 'Профессия'}, 
+  {inputOne: 'Название', inputTwo: 'Ссылка на картинку'},]
+
 const popUpContainer = document.querySelector('.body')
+const currentAccountName = popUpContainer.querySelector('.profile__title')
+const currentAccountProfession = popUpContainer.querySelector('.profile__subtitle')
+const cardContainer = popUpContainer.querySelector('.elements')
+const bigPicturePopup = popUpContainer.querySelector('#big-picture-popup')
+const bigPicturePopupImage = bigPicturePopup.querySelector('.big-picture__image')
+const bigPicturePopupSpan = bigPicturePopup.querySelector('.big-picture__tag')
+const cardFromTemplate = popUpContainer.querySelector('#elements__item-template').content.querySelector('.elements__item')
+const popupAccountEditModifier = popUpContainer.querySelector('#profileEditPopup')
+const popupEditAccountFormTitleCurrent = popupAccountEditModifier.querySelector('.popup__title')
+const profileEditFormCredentials = popupAccountEditModifier.querySelector('.credentials')
+const profileEditFormInitialValue1 = profileEditFormCredentials.querySelector('#formInitialValue1')
+const profileEditFormInitialValue2 = profileEditFormCredentials.querySelector('#formInitialValue2')
+const profileEditButton = popUpContainer.querySelector('.profile__edit-button')
+const popupAccountNewCardModifier = popUpContainer.querySelector('#newCardPopup')
+const popupNewCardFormTitleCurrent = popupAccountNewCardModifier.querySelector('.popup__title')
+const profileNewCardFormCredentials = popupAccountNewCardModifier.querySelector('.credentials')
+const profileNewCardFormInitialValue1 = profileNewCardFormCredentials.querySelector('#newCardValue1')
+const profileNewCardFormInitialValue2 = profileNewCardFormCredentials.querySelector('#newCardValue2')
+const newCardCreatorButton = popUpContainer.querySelector('.profile__add-button')
 
-const currentAccountName = document.querySelector('.profile__title')
+activatePopup = popup => {  
+  popup.classList.remove('popup_animation-out')
+  popup.classList.add('popup_animation-on')
+}
 
-const currentAccountProfession = document.querySelector('.profile__subtitle')
+removePopup = popup => {  
+  popup.classList.remove('popup_animation-on')
+  popup.classList.add('popup_animation-out')
+}
 
-const cardContainer = document.querySelector('.elements')
-
-const cardFromTemplate = document.querySelector('#elements__item-template').content.querySelector('.elements__item')
-
-function renderElementCard (heading) {
+renderElementCard = heading => {
   const cardCreator = cardFromTemplate.cloneNode(true)
   const cardCreatorTextElement = cardCreator.querySelector('.elements__heading')
-  cardCreatorTextElement.textContent = heading['title']
   const cardCreatorImageElement = cardCreator.querySelector('.elements__image')
+  cardCreatorTextElement.textContent = heading['title']
   cardCreatorImageElement.setAttribute('src', heading['link'])  
-  cardContainer.prepend(cardCreator)
+  cardCreatorImageElement.setAttribute('alt', heading['title'])     
 
   const cardRemover = cardCreator.querySelector('.elements__delete-button')
   cardRemover.addEventListener('click', evt => {evt.target.closest('.elements__item').remove()})
@@ -37,65 +65,35 @@ function renderElementCard (heading) {
     evt.target.closest('.elements__like-button').classList.toggle('elements__like-button_active')
   })
 
-  const bigPicturePopup = document.querySelector('.big-picture')
-  cardCreatorImageElement.addEventListener('click', () => {
-    const bigPicturePopupImage = bigPicturePopup.querySelector('.big-picture__image')
+  cardCreatorImageElement.addEventListener('click', () => {    
     bigPicturePopupImage.setAttribute('src', heading['link'])
-    const bigPicturePopupSpan = bigPicturePopup.querySelector('.big-picture__tag')
+    bigPicturePopupImage.setAttribute('alt', heading['title'])     
     bigPicturePopupSpan.textContent = heading['title']
-    bigPicturePopup.classList.remove('big-picture_animation-out')
-    bigPicturePopup.classList.add('big-picture_animation-on')
+    activatePopup(bigPicturePopup)
   })
 
-  const bigPictureRemover = document.querySelector('.big-picture__close-button')
-  bigPictureRemover.addEventListener('click', () => {
-    bigPicturePopup.classList.remove('big-picture_animation-on')
-    bigPicturePopup.classList.add('big-picture_animation-out')
-  })
+  return cardCreator
 }
 
-cardContentTitles.forEach(renderElementCard)
+addCard = (title, link) => {
+  const cardCaller = renderElementCard(title, link)
+  cardContainer.prepend(cardCaller) 
+}
+
+cardContentTitles.forEach(addCard)
+
+bigPicturePopup.querySelector('.popup__close-button').addEventListener('click', () => {
+  removePopup(bigPicturePopup)})
 
 // 2 Popup containers / not-from-template method
 
-const popupTitle = 
-['Редактировать профиль', 'Новое место']
-
-const popupFormPlaceholder = 
-[ {inputOne: 'Имя исследователя', inputTwo: 'Профессия'}, 
-  {inputOne: 'Название', inputTwo: 'Ссылка на картинку'}, ]
-
-const popupEditFormInitialValue = 
-[{inputOne: 'Жак-Ив Кусто', inputTwo: 'Исследователь океана'}]
-
-// Edit-popup variables
-
-const popupAccountEditModifier = document.querySelector('#profileEditPopup')
-const popupEditAccountFormTitleCurrent = popupAccountEditModifier.querySelector('.popup__title')
-const profileEditFormCredentials = popupAccountEditModifier.querySelector('.credentials')
-const profileEditFormInitialValue1 = profileEditFormCredentials.querySelector('#formInitialValue1')
-const profileEditFormInitialValue2 = profileEditFormCredentials.querySelector('#formInitialValue2')
-const profileEditCloseButton = popupAccountEditModifier.querySelector('.popup__close-button')
-const profileEditButton = document.querySelector('.profile__edit-button')
-
-// New-Card-popup variables
-
-const popupAccountNewCardModifier = document.querySelector('#newCardPopup')
-const popupNewCardFormTitleCurrent = popupAccountNewCardModifier.querySelector('.popup__title')
-const profileNewCardFormCredentials = popupAccountNewCardModifier.querySelector('.credentials')
-const profileNewCardFormInitialValue1 = profileNewCardFormCredentials.querySelector('#newCardValue1')
-const profileNewCardFormInitialValue2 = profileNewCardFormCredentials.querySelector('#newCardValue2')
-const profileNewCardCloseButton = popupAccountNewCardModifier.querySelector('.popup__close-button')
-const newCardCreatorButton = document.querySelector('.profile__add-button')
-
 // Edit button activating popup and submit button closing popup
 
-profileEditButton.addEventListener('click', function() {
-  popupAccountEditModifier.classList.remove('popup_animation-out')
-  popupAccountEditModifier.classList.add('popup_animation-on')
+profileEditButton.addEventListener('click', () => {
+  activatePopup(popupAccountEditModifier)
   popupEditAccountFormTitleCurrent.textContent = popupTitle[0]
-  profileEditFormInitialValue1.setAttribute('value', popupEditFormInitialValue[0]['inputOne'])
-  profileEditFormInitialValue2.setAttribute('value', popupEditFormInitialValue[0]['inputTwo'])
+  profileEditFormInitialValue1.setAttribute('value', currentAccountName.textContent)
+  profileEditFormInitialValue2.setAttribute('value', currentAccountProfession.textContent)
   profileEditFormInitialValue1.setAttribute('placeholder', popupFormPlaceholder[0]['inputOne'])
   profileEditFormInitialValue2.setAttribute('placeholder', popupFormPlaceholder[0]['inputTwo'])
 })
@@ -104,26 +102,20 @@ profileEditFormCredentials.addEventListener('submit', evt => {
   evt.preventDefault()
   const pendingInputValue1 = evt.target.querySelector('#formInitialValue1').value
   currentAccountName.textContent = pendingInputValue1
-  popupEditFormInitialValue[0]['inputOne'] = pendingInputValue1
   const pendingInputValue2 = evt.target.querySelector('#formInitialValue2').value
   currentAccountProfession.textContent = pendingInputValue2
-  popupEditFormInitialValue[0]['inputTwo'] = pendingInputValue2
-  popupAccountEditModifier.classList.remove('popup_animation-on')
-  popupAccountEditModifier.classList.add('popup_animation-out')
+  removePopup(popupAccountEditModifier)
 })
 
-profileEditCloseButton.addEventListener('click', () => {
-  popupAccountEditModifier.classList.remove('popup_animation-on')
-  popupAccountEditModifier.classList.add('popup_animation-out')
-  })
+popupAccountEditModifier.querySelector('.popup__close-button').addEventListener('click', () => {
+  removePopup(popupAccountEditModifier)})
 
 
 // New Card button activating popup and submit button closing popup
 
 
 newCardCreatorButton.addEventListener('click', () => {
-  popupAccountNewCardModifier.classList.remove('popup_animation-out')
-  popupAccountNewCardModifier.classList.add('popup_animation-on')
+  activatePopup(popupAccountNewCardModifier)
   popupNewCardFormTitleCurrent.textContent = popupTitle[1]
   profileNewCardFormInitialValue1.setAttribute('placeholder', popupFormPlaceholder[1]['inputOne'])
   profileNewCardFormInitialValue2.setAttribute('placeholder', popupFormPlaceholder[1]['inputTwo'])
@@ -131,18 +123,16 @@ newCardCreatorButton.addEventListener('click', () => {
   
 profileNewCardFormCredentials.addEventListener('submit', evt => {
   evt.preventDefault()
-  let newCardInputValue1 = evt.target.querySelector('#newCardValue1')
-  let newCardInputValue2 = evt.target.querySelector('#newCardValue2')
+  const newCardInputValue1 = evt.target.querySelector('#newCardValue1')
+  const newCardInputValue2 = evt.target.querySelector('#newCardValue2')
   cardContentTitles.push({title: `${newCardInputValue1.value}`, link:`${newCardInputValue2.value}`})
   const lastInputCredentials = cardContentTitles.pop()
-  renderElementCard(lastInputCredentials)
+  addCard(lastInputCredentials)
   newCardInputValue1.value = ''
   newCardInputValue2.value = ''
-  popupAccountNewCardModifier.classList.remove('popup_animation-on')
-  popupAccountNewCardModifier.classList.add('popup_animation-out')
+  removePopup(popupAccountNewCardModifier)
 })  
   
-profileNewCardCloseButton.addEventListener('click', () => {
-  popupAccountNewCardModifier.classList.remove('popup_animation-on')
-  popupAccountNewCardModifier.classList.add('popup_animation-out')
-  })  
+popupAccountNewCardModifier.querySelector('.popup__close-button').addEventListener('click', () => {
+  removePopup(popupAccountNewCardModifier)
+})
